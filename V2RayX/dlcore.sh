3d1b0c6b8da695fa3c35fe1ab6e1ff6bc6e120a1
Cenmrev <Cenmrev@users.noreply.github.com>
2016-10-16 14:05:58 -0400

    download v2ray binary automatically when building

---
+++ V2RayX/dlcore.sh
@@ -0,0 +1,27 @@
+VERSION="v2.2.1"
+
+
+cd $SRCROOT
+output="v0"
+if [[ -f ./v2ray-core-bin/v2ray ]]; then
+    echo "exit"
+    output=$(./v2ray-core-bin/v2ray --version)
+fi
+existingVersion=${output:6:${#VERSION}}
+if [ "$VERSION" != "$existingVersion" ]; then
+    curl -s -L --create-dirs -o v2ray-core-bin/v2ray-macos.zip https://github.com/v2ray/v2ray-core/releases/download/${VERSION}/v2ray-macos.zip
+    if [[ $? == 0 ]]; then
+        cd v2ray-core-bin
+        unzip -o v2ray-macos.zip
+        mv v2ray-${VERSION}-macos/v2ray v2ray
+        chmod +x ./v2ray
+        rm -r v2ray-*
+        exit 0
+    else
+        echo "download failed!"
+        exit 1
+    fi
+else 
+    exit 0
+fi
+
