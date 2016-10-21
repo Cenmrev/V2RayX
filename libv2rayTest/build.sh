1e55683d745b09fad0cf944abec53be09722c15b
Cenmrev <Cenmrev@users.noreply.github.com>
2016-10-20 21:59:03 -0400

    add linux option

--- libv2rayTest/build.sh
+++ libv2rayTest/build.sh
@@ -1,1 +1,6 @@
-go build -x -tags json -buildmode=c-shared -o libv2ray.dylib ./main.go
+if [ "$(uname)" == "Darwin" ]; then
+    go build -x -tags json -buildmode=c-shared -o libv2ray.dylib ./main.go        
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    go build -x -tags json -buildmode=c-shared -o libv2ray.so ./main.go
+fi
+
