e94732e8575dac604952239e24152ca9a927c5fb
Chandler-Lu <chandler.lu1997@gmail.com>
2018-05-06 23:24:03 +0800

    add none

--- V2RayX/ServerProfile.m
+++ V2RayX/ServerProfile.m
@@ -67,7 +67,7 @@
 
 + (ServerProfile*)readFromAnOutboundDic:(NSDictionary*)outDict {
     NSDictionary *netWorkDict = @{@"tcp": @0, @"kcp": @1, @"ws":@2 };
-    NSDictionary *securityDict = @{@"aes-128-cfb":@0, @"aes-128-gcm":@1, @"chacha20-poly1305":@2, @"auto":@3};
+    NSDictionary *securityDict = @{@"aes-128-cfb":@0, @"aes-128-gcm":@1, @"chacha20-poly1305":@2, @"auto":@3, @"none":@4};
     
     ServerProfile* profile = [[ServerProfile alloc] init];
     profile.sendThrough = nilCoalescing(outDict[@"sendThrough"], @"0.0.0.0");
@@ -109,7 +109,7 @@
                                   @{
                                       @"id": userId != nil ? [userId stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]: @"",
                                       @"alterId": [NSNumber numberWithUnsignedInteger:alterId],
-                                      @"security": @[@"aes-128-cfb", @"aes-128-gcm", @"chacha20-poly1305", @"auto"][security],
+                                      @"security": @[@"aes-128-cfb", @"aes-128-gcm", @"chacha20-poly1305", @"auto", @"none"][security],
                                       @"level": [NSNumber numberWithUnsignedInteger:level]
                                       }
                                   ]
